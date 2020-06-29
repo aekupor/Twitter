98694ebfd0c7b97678694c8390a2813f2f45af02
Ashlee Kupor <aekupor@gmail.com>
2020-06-29 14:22:24 -0700

    [Compose] Add varibles to ComposeActivity.

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -3,12 +3,20 @@ package com.codepath.apps.restclienttemplate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ComposeActivity extends AppCompatActivity {
+
+    EditText etCompose;
+    Button btnTweet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
+
+        etCompose = findViewById(R.id.etCompose);
+        btnTweet = findViewById(R.id.btnTweet);
     }
 }
