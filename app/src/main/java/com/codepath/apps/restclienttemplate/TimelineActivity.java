70af55e1d1d3284c518755d58646393f6a7634d5
Ashlee Kupor <aekupor@gmail.com>
2020-06-29 14:26:31 -0700

    [Compose] Create intent to open ComposeActivity.

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -80,6 +81,8 @@ public class TimelineActivity extends AppCompatActivity {
             //compose icon has been selected
             Toast.makeText(this, "Compose", Toast.LENGTH_SHORT).show();
             //navigate to the compose activity
+            Intent intent = new Intent(this, ComposeActivity.class);
+            startActivity(intent);
             return true;
         }
         return super.onOptionsItemSelected(item);
