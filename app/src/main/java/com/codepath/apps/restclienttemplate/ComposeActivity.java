b1b8a86cca6c6f63c6458194bcebeb2118ed75b9
Ashlee Kupor <aekupor@gmail.com>
2020-06-29 14:25:06 -0700

    [Compose] Create onClick listener for tweet button.

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -3,8 +3,10 @@ package com.codepath.apps.restclienttemplate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ComposeActivity extends AppCompatActivity {
 
@@ -18,5 +20,23 @@ public class ComposeActivity extends AppCompatActivity {
 
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
+
+        //set click listener on button
+        btnTweet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tweetContent = etCompose.getText().toString();
+                if (tweetContent.isEmpty()) {
+                    Toast.makeText(ComposeActivity.this, "Sorry, your tweet cannot be empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (tweetContent.length() > 140) {
+                    Toast.makeText(ComposeActivity.this, "Sorry, your tweet is too long", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //make an API call to Twitter to publish the tweet
+                Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
