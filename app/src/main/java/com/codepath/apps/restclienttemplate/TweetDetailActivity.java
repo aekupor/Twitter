b7c7a3bf5e3cb989e92744bb6289d36696863ffd
Ashlee Kupor <aekupor@gmail.com>
2020-06-30 10:43:51 -0700

    [Tweet Details] Add variables to TweetDetailActivity and unwrap tweet object.

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetDetailActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetDetailActivity.java
@@ -3,12 +3,38 @@ package com.codepath.apps.restclienttemplate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import org.parceler.Parcels;
 
 public class TweetDetailActivity extends AppCompatActivity {
+
+    public static final String TAG = "TweetDetailActivity";
+
+    TextView tvScreenName;
+    TextView tvBody;
+    TextView tvDate;
+    ImageView ivProfileImage;
+    Tweet tweet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tweet_detail);
+
+        tvScreenName = findViewById(R.id.tvScreenName);
+        tvBody = findViewById(R.id.tvBody);
+        tvDate = findViewById(R.id.tvDate);
+        ivProfileImage = findViewById(R.id.ivProfileImage);
+
+        // unwrap the movie passed in via intent
+        tweet = (Tweet) Parcels.unwrap(getIntent().getParcelableExtra("tweet"));
+        Log.d(TAG, String.format("Showing details for '%s'", tweet.body));
     }
 }
