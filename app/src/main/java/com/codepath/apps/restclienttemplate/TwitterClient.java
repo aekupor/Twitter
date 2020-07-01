6aa86d6e09f4191024f27a575403ec47cddc08e1
Ashlee Kupor <aekupor@gmail.com>
2020-07-01 14:04:56 -0700

    [Followers] Add getFollowersList API call.

--- app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
@@ -103,6 +103,13 @@ public class TwitterClient extends OAuthBaseClient {
 		client.post(apiUrl, params, "", handler);
 	}
 
+	public void getFollowersList(long userId, JsonHttpResponseHandler handler) {
+		String apiUrl = getApiUrl("followers/list.json");
+		RequestParams params = new RequestParams();
+		params.put("user_id", userId);
+		client.get(apiUrl, params, handler);
+	}
+
 	/* 1. Define the endpoint URL with getApiUrl and pass a relative path to the endpoint
 	 * 	  i.e getApiUrl("statuses/home_timeline.json");
 	 * 2. Define the parameters to pass to the request (query or body)
