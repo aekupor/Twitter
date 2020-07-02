f779989c6c89d1fb23396c75d030f3e4640d99b2
Ashlee Kupor <aekupor@gmail.com>
2020-07-02 10:54:04 -0700

    [User Details] Connect data to UserDetailActivity.

--- app/src/main/java/com/codepath/apps/restclienttemplate/UserDetailActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/UserDetailActivity.java
@@ -3,12 +3,37 @@ package com.codepath.apps.restclienttemplate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.apps.restclienttemplate.models.User;
+
+import org.parceler.Parcels;
 
 public class UserDetailActivity extends AppCompatActivity {
+
+    TextView tvName;
+    TextView tvScreenName;
+    ImageView ivProfileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_detail);
+
+        tvName = findViewById(R.id.tvName);
+        tvScreenName = findViewById(R.id.tvScreenName);
+        ivProfileImage = findViewById(R.id.ivProfileImage);
+
+        User user = (User) Parcels.unwrap(getIntent().getParcelableExtra("USER"));
+
+        tvName.setText(user.name);
+        tvScreenName.setText("@" + user.screenName);
+
+        Glide.with(this)
+                .load(user.profileImageUrl)
+                .into(ivProfileImage);
     }
 }
