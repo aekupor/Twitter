432dbc1069a84108ddd4d73146309ab3237812a5
Ashlee Kupor <aekupor@gmail.com>
2020-07-02 11:36:08 -0700

    [User Details] Add user tweets to UserDetailActivity.

--- app/src/main/java/com/codepath/apps/restclienttemplate/UserDetailActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/UserDetailActivity.java
@@ -1,24 +1,42 @@
 package com.codepath.apps.restclienttemplate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.apps.restclienttemplate.models.User;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.parceler.Parcels;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
+
 public class UserDetailActivity extends AppCompatActivity {
+
+    public static final String TAG = "UserDetailActivity";
 
     TextView tvName;
     TextView tvScreenName;
     TextView tvDescription;
     TextView tvTweetsNum;
     ImageView ivProfileImage;
+
+    TwitterClient client;
+    RecyclerView rvTweets;
+    List<Tweet> tweets;
+    TweetsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +59,40 @@ public class UserDetailActivity extends AppCompatActivity {
         Glide.with(this)
                 .load(user.profileImageUrl)
                 .into(ivProfileImage);
+
+        client = TwitterApp.getRestClient(this);
+
+        //find the recycler view
+        rvTweets = findViewById(R.id.rvTweets);
+        //init the list of tweets and adapter
+        tweets = new ArrayList<>();
+        adapter = new TweetsAdapter(this, tweets);
+        //recycler view setup: layout manager and the adapter
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        rvTweets.setLayoutManager(layoutManager);
+        rvTweets.setAdapter(adapter);
+
+        populateUserTimeline(user);
+    }
+
+    private void populateUserTimeline(User user) {
+        client.getUserTweets(user.idInt, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.i(TAG, "onSuccess" + json.toString());
+                JSONArray jsonArray = json.jsonArray;
+                try {
+                    adapter.clear();
+                    adapter.addAll(Tweet.fromJsonArray(jsonArray));
+                } catch (JSONException e) {
+                    Log.e(TAG, "json exception", e);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.i(TAG, "onFailure", throwable);
+            }
+        });
     }
 }
