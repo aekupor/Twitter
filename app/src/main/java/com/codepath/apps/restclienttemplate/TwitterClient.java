7ea92bf600a07e260f02c6cbd3a2e8a0e3ee112f
Ashlee Kupor <aekupor@gmail.com>
2020-07-02 13:49:07 -0700

    [Profile] Create new API call to see likes.

--- app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
@@ -134,6 +134,13 @@ public class TwitterClient extends OAuthBaseClient {
 		client.get(apiUrl, params, handler);
 	}
 
+	public void getUserLikes(JsonHttpResponseHandler handler) {
+		String apiUrl = getApiUrl("favorites/list.json");
+		RequestParams params = new RequestParams();
+		params.put("count", 200);
+		client.get(apiUrl, params, handler);
+	}
+
 	/* 1. Define the endpoint URL with getApiUrl and pass a relative path to the endpoint
 	 * 	  i.e getApiUrl("statuses/home_timeline.json");
 	 * 2. Define the parameters to pass to the request (query or body)
